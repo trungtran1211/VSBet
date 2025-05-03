b99c595b7f3ab9f60cba88dc8a3d9a80f8fcb887
trungtran1211 <thanhtrungtran8888@gmail.com>
2025-05-03 18:56:23 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,41 @@
+//scroll section
+
+document.addEventListener('DOMContentLoaded', function () {
+    if (window.innerWidth > 768) {
+        new fullpage('#fullpage', {
+            anchors: ['ve-chung-toi', 'truc-tiep-bong-da-1', 'truc-tiep-bong-da-2', 'hoat-dong-xa-hoi'],
+            navigation: true,
+            onLeave: function (origin, destination, direction) {
+                // chạy animation ở đây
+            }
+        });
+    }
+});
+
+const menu = document.querySelector('.menu');
+const bgMenu = document.querySelector('.bg-menu');
+const headerBt = document.querySelector('.header-bt-mb');
+const close = document.querySelector('.close');
+const listMenuItems = document.querySelectorAll('.list-menu > ul > li > a');
+
+menu.addEventListener('click', () => {
+bgMenu.style.display = 'block';
+headerBt.classList.add('active');
+});
+
+bgMenu.addEventListener('click', () => {
+bgMenu.style.display = 'none';
+headerBt.classList.remove('active');
+});
+
+close.addEventListener('click', () => {
+bgMenu.style.display = 'none';
+headerBt.classList.remove('active');
+});
+
+listMenuItems.forEach(item => {
+item.addEventListener('click', () => {
+    bgMenu.style.display = 'none';
+    headerBt.classList.remove('active');
+});
+});
